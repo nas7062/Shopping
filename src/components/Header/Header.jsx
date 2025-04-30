8aff3aee96289a6484b6d103f52de70f0ca27bd0
10012 <nas7062@naver.com>
2025-04-30 23:27:53 +0900

    feat: 헤더 추가

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import { Link, NavLink } from 'react-router-dom'
+import styles from './header.module.css'
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1>
+        <Link to={'/'}>10012</Link>
+      </h1>
+      <nav>
+        <NavLink to={'/'}>BEST</NavLink>
+        <NavLink to={'/'}>TOP</NavLink>
+        <NavLink to={'/'}>OUTER</NavLink>
+        <NavLink to={'/'}>BOTTOM</NavLink>
+        <NavLink to={'/'}>SHOES</NavLink>
+      </nav>
+      <nav>
+        <NavLink to={'/'}>SEARCH</NavLink>
+        <NavLink to={'/'}>CART</NavLink>
+        <NavLink to={'/'}>MYPAGE</NavLink>
+      </nav>
+    </header>
+  )
+}
+export default Header
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from 'react-router-dom'
 import styles from './mainlayout.module.css'
+import Header from '../components/Header/Header'
 
 const MainLayout = () => {
   return (
     <main className={styles.container}>
-      <h2>MainLayout</h2>
+      <Header />
       <Outlet />
     </main>
   )
