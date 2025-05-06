b2018bd2ac72e32284ec7919247530e0bbbe169a
kim min seok <nas7062@naver.com>
2025-05-06 21:46:03 +0900

    json 서버 설정 및 db.json 추가

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,12 +1,24 @@
 import css from './slider.module.css'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react'
-
 import { Navigation, Pagination } from 'swiper/modules'
+import axios from 'axios'
 export const Slider = () => {
-  const arr = new Array(12).fill(0).map((_, i) => i + 1)
-  console.log(arr)
+  const [product, setProduct] = useState([])
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/products')
+        console.log(response)
+        setProduct(response.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchProduct()
+  }, [])
+  console.log(product)
   return (
     <main className={css.slider}>
       <>
@@ -19,10 +31,24 @@ export const Slider = () => {
             delay: 1000,
             disableOnInteraction: false,
           }}
+          breakpoints={{
+            1200: {
+              slidesPerView: 3,
+              slidesPerGroup: 3,
+            },
+            700: {
+              slidesPerView: 2,
+              slidesPerGroup: 2,
+            },
+            400: {
+              slidesPerView: 1,
+              slidesPerGroup: 1,
+            },
+          }}
           modules={[Navigation]}
           className="mySwiper"
         >
-          {arr.map((item, idx) => (
+          {product.map((item, idx) => (
             <SwiperSlide key={idx}>
               <img src={`/src/assets/slider${item}.jpg`} alt={`슬라이더 사진${item}`} />
             </SwiperSlide>
---
+++ src/components/Slider/SliderItem.jsx
@@ -0,0 +1,9 @@
+import css from './slideritem.module.css'
+
+export const SliderItem = () => {
+  return (
+    <main className={css.slideritem}>
+      <p>SliderItem</p>
+    </main>
+  )
+}
